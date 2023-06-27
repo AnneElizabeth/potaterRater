42954a748efff415d16559bf62c075ade3ee5020
Anne Edmondson <edmondson.anne@gmail.com>
2023-06-26 21:42:30 -0600

    Commit code for data display, renamed db.json data

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetchPotato()
-})
+ document.addEventListener('DOMContentLoaded', () => {
+        fetchPotato()
+    })
 
-// variables
-
-
-// data requests
+// data request
 
 function fetchPotato() {
     fetch("http://localhost:3000/potatoes")
     .then(resp => resp.json())
-    .then(data => 
-        addInfoCard(data))
+    .then(data => {
+        potatoArray = data
+        potatoArray.forEach(addDetailCard)
+    })
 }
+
 
 // functions
 
-function addInfoCard() {
-    let infoCard = document.querySelector('.card')
-    infoCard.innerText +=
+function addDetailCard(potatoes) {
+    let detailCard = document.querySelector(".row")
+    detailCard.innerHTML +=
         `
-            <img src="${potatoes.img}" class="card-img-top" alt="All Blue">
-            <div class="card-body">
+        <div class="col"
+            <div class="card">
+                <img src="${potatoes.img}" class="card-img-top" alt="">
+                <div class="card-body">
                 <h5 class="card-title">${potatoes.name}</h5>
-                <p class="card-text"><strong>SHAPE:</strong> ${potatoes.shape}</p>
-                <p class="card-text"><strong>COLOR:</strong> ${potatoes.color}</p>
-                <p class="card-text"><strong>FLAVOR:</strong> ${potatoes.flavor}</p>
-                <p class="card-text"><strong>USAGE:</strong> ${potatoes.shape}</p>
-            </div>
+                    <p class="card-text"><strong>SHAPE:</strong> ${potatoes.shape}</p>
+                    <p class="card-text"><strong>COLOR:</strong> ${potatoes.color}</p>
+                    <p class="card-text"><strong>FLAVOR:</strong> ${potatoes.flavor}</p>
+                    <p class="card-text"><strong>USAGE:</strong> ${potatoes.shape}</p> 
+                </div>
+            </div> 
+        </div>   
         `
 }
 
